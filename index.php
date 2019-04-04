b3540594a946ca66ef0ab5fc433739cccef65af7
mgallardo3 <mgallardo3@mail.greenriver.edu>
2019-04-03 23:55:55 -0400

    added an index page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair programming 1</title>
+</head>
+<body>
+    <h1>Pair programming 1</h1>
+
+</body>
+</html>
+<?php
+
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+
+
