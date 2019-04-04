dbc57ccf17914c463bec860caeae56e00b3ae76d
mgallardo3 <mgallardo3@mail.greenriver.edu>
2019-04-04 00:02:59 -0400

    added a functions.php file

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: homefolder
+ * Date: 2019-04-03
+ * Time: 21:01
+ */
+
+function printArr($array)
+{
+    foreach($array as $value)
+    {
+        echo $value . " ";
+    }
+}
